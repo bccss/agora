f137c47386697e56eaafe579dc4e4d6dfe53859e
Rootul Patel <rootulp@gmail.com>
2015-01-17 01:10:42 -0500

    comment out smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,13 +58,13 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV.fetch("ASSET_HOST")
+  # config.action_controller.asset_host = ENV.fetch("ASSET_HOST")
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = SMTP_SETTINGS
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = SMTP_SETTINGS
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
