ca27a449b0aa729f69f5ee9ed30b1fd59c168549
jmu303 <muj@bc.edu>
2015-01-17 23:39:47 -0500

    Add authenticate_user_from_token method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
+  def authenticate_user_from_token!
+    auth_token = request.headers['X-AUTH-TOKEN']
+    user = auth_token && User.find_by(authentication_token: auth_token)
+
+    if user
+      # Notice we are passing store false, so the user is not
+      # actually stored in the session and a token is needed
+      # for every request. If you want the token to work as a
+      # sign in token, you can simply remove store: false.
+      sign_in user, store: false
+    end
+  end
 end
